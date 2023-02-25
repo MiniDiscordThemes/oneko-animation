32aade2218a6400e705443972b83820ee53ca720
OIRNOIR <oirnoir@outlook.com>
2023-02-25 13:27:51 -0800

    More animation tweaks

--- generate-run-keyframes.js
+++ generate-run-keyframes.js
@@ -4,24 +4,24 @@
 
 let lines = [];
 
-for (let i = 0; i < 15; i++) {
-	const startingPercentage = i == 0 ? 0 : ((i * 50) / 15 + 0.0001).toFixed(4);
-	const endingPercentage = (((i + 1) * 50) / 15).toFixed(4);
-	lines.push(`    ${startingPercentage}%, ${endingPercentage}% {`);
+for (let i = 0; i < 20; i++) {
+	const startingPercentage = i == 0 ? 0 : ((i * 66) / 20 + 0.0001).toFixed(4);
+	const endingPercentage = (((i + 1) * 66) / 20).toFixed(4);
+	lines.push(`  ${startingPercentage}%, ${endingPercentage}% {`);
 	if (i < 2) {
-		lines.push(`        background-position: -96px -${(i % 2) * 32 + 64}px;`);
+		lines.push(`    background-position: -96px -${(i % 2) * 32 + 64}px;`);
 	} else {
-		lines.push(`        background-position: -64px -${(i % 2) * 32}px;`);
+		lines.push(`    background-position: -64px -${(i % 2) * 32}px;`);
 	}
-	lines.push("    }");
+	lines.push("  }");
 }
 
 for (let i = 0; i < 120; i++) {
-	const startingPercentage = ((i * 50) / 120 + 50.0001).toFixed(4);
-	const endingPercentage = (((i + 1) * 50) / 120 + 50).toFixed(4);
-	lines.push(`    ${startingPercentage}%, ${endingPercentage}% {`);
-	lines.push(`        background-position: -128px -${(i % 4) * 32}px;`);
-	lines.push("    }");
+	const startingPercentage = ((i * 34) / 120 + 66.0001).toFixed(4);
+	const endingPercentage = (((i + 1) * 34) / 120 + 66).toFixed(4);
+	lines.push(`  ${startingPercentage}%, ${endingPercentage}% {`);
+	lines.push(`    background-position: -128px -${(i % 4) * 32}px;`);
+	lines.push("  }");
 }
 
 console.log(lines.join("\n"));
